cf0be610d264e1e6e125154854231c40f28e9174
Yusef Napora <yusef.napora@sharefile.com>
2025-08-13 11:20:52 -0400

    only iterate once: -18 chars

--- roman.py
+++ roman.py
@@ -11,13 +11,13 @@ for l in open(sys.argv[1]):
     v=1000
     while n > 0:
         t[i], n = divmod(n, v)
-        v=v//D[i%2]
-        i+=1
+        v//=D[i%2]
 
-    for i in range(7):
         if t[i] < 4:
             s += A[i] * t[i]
         else:
             y = t[i-1]
             s = s[:len(s)-y] + A[i] + A[i-1-y]
+
+        i+=1
     print(s)
