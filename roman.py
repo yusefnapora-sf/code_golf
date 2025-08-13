bebd67320bd8cc198981de22fa1dfb4c66bed2d4
Yusef Napora <yusef.napora@sharefile.com>
2025-08-13 11:16:02 -0400

    ditch with / as to save 14 chars :)

--- roman.py
+++ roman.py
@@ -3,22 +3,21 @@ import sys
 A=list('MDCLXVI')
 D=(2,5)
 
-with open(sys.argv[1]) as f:
-    for l in f:
-        n=int(l)
-        s=''
-        t=[0] * 7
-        i=0
-        v=1000
-        while n > 0:
-            t[i], n = divmod(n, v)
-            v=v//D[i%2]
-            i+=1
+for l in open(sys.argv[1]):
+    n=int(l)
+    s=''
+    t=[0] * 7
+    i=0
+    v=1000
+    while n > 0:
+        t[i], n = divmod(n, v)
+        v=v//D[i%2]
+        i+=1
 
-        for i in range(7):
-            if t[i] < 4:
-                s += A[i] * t[i]
-            else:
-                y = t[i-1]
-                s = s[:len(s)-y] + A[i] + A[i-1-y]
-        print(s)
+    for i in range(7):
+        if t[i] < 4:
+            s += A[i] * t[i]
+        else:
+            y = t[i-1]
+            s = s[:len(s)-y] + A[i] + A[i-1-y]
+    print(s)
